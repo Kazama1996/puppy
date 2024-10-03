7991a9b4ee92a5986811b2f5787d00abf163c759
kazama <y.kazama1996@gmail.com>
2024-10-03 21:03:47 +0800

    create bark and barks method

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Kazama1996/puppy
+
+go 1.23.2
---
+++ puppy.go
@@ -0,0 +1,10 @@
+package puppy
+
+func Bark() string {
+	return "woof!"
+}
+
+func Barks() string {
+
+	return "woof woof wooffff"
+}
